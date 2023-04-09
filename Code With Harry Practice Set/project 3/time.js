238f2f34d6eb26fa705a1520f6a1bf2ee1592162
skr18 <sujeetrath123@gmail.com>
2023-04-09 20:24:54 +0530

    project 3

--- Code With Harry Practice Set/project 3/time.js
+++ Code With Harry Practice Set/project 3/time.js
@@ -1,3 +1,8 @@
-setInterval(() => {
-    console.log("hello")
-}, 3000);
+// let a = document.getElementsByTagName('div')
+// console.log(a[0])
+// a[0].style.backgroundColor='red';
+
+// setInterval(() => {
+//     a.style.backgroundColor='red'
+//     a.style.backgroundColor='green'
+// }, 500);
